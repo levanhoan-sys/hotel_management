6eae2ec95968342fbdaff5b5da4c45cd1d8c9d60
levanhoan <levanhoan10a1@gmail.com>
2026-09-21 09:29:47 +0700

    Initial commit

---
+++ hotel/src/test/java/com/nhom7/hotel/HotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom7.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
