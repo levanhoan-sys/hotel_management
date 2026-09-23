aade9f59f78f51aa3ff4ef1b71cf4351e487782a
levanhoan <levanhoan10a1@gmail.com>
2026-09-23 17:15:13 +0700

    add user

---
+++ backend/src/main/java/com/nhom7/hotel/entity/User.java
@@ -0,0 +1,24 @@
+package com.nhom7.hotel.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+    String username;
+    String password;
+    String email;
+    String phone;
+}
